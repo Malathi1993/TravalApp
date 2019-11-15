c4af05adc6275548891c065b9d0f6823052a86d1
Malathi <57785954+Malathi1993@users.noreply.github.com>
2019-11-15 12:11:40 +0530

    dummy

--- app/src/main/java/com/example/travalapp/activity/EditProfileActivity.java
+++ app/src/main/java/com/example/travalapp/activity/EditProfileActivity.java
@@ -8,9 +8,5 @@ import com.example.travalapp.R;
 
 public class EditProfileActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_editprofile);
-    }
+   
 }
